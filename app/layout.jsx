157001c49ff216054a5d34f5e225148d4f3fa0ee
Muh Iqbal <muhammadiqbal18081996@gmail.com>
2023-05-11 23:31:14 +0800

    feat : menambahkan navbar dan login auth google

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,13 +11,15 @@ const RootLayout = ({ children }) => {
   return (
     <html lang='en'>
         <body>
+            <Provider>
             <div className='main'>
                 <div className='gradient'/>
             </div>
             <main className='app'>
                 <Nav />
                 {children}
-            </main>
+            </main>          
+            </Provider>
         </body>
     </html>
   )
